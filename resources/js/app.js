52cbf8dfff10d7a394b7745d183e015a7cbd71a4
Kristian Engberg <krissoengberg@gmail.com>
2022-04-03 15:09:17 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+require('./bootstrap')
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+
+
+axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.withCredentials = true
+
+
+createApp(App).use(router).mount('#app')
---
+++ resources/js/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>VUE HOME</div>
+</template>
+
+<script>
+import axios from 'axios'
+import { ref } from '@vue/reactivity'
+
+export default {
+
+    setup() {
+
+        const value = ref({})
+
+        const test = async () => {
+            let response = await axios.get('api/users')
+            value.value = response.data
+        }
+
+        test()
+
+        return {
+
+        }
+    }
+
+}
+</script>
+
+<style>
+</style>
